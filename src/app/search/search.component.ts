1b884e15bf6022f0b681bb5ceaea7125d2721c36
Katarzyna <katarzyna.skibinska94@gmail.com>
2025-12-02 15:09:13 +0100

    search-component

---
+++ src/app/search/search.component.ts
@@ -0,0 +1,19 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-search',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './search.component.html',
+  styleUrl: './search.component.scss',
+})
+export class SearchComponent {
+  @Input() query: string = '';
+  @Input() placeholder: string = '';
+  @Output() search = new EventEmitter<string>();
+
+  onSearch() {
+    this.search.emit(this.query);
+  }
+}

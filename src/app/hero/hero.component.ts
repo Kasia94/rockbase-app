fffd400157130d0e44ec41d4aaf594538352f22c
Katarzyna <katarzyna.skibinska94@gmail.com>
2025-12-02 15:06:50 +0100

    hero-component

---
+++ src/app/hero/hero.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { AlbumsSliderComponent } from '../albums/albums-slider.component';
+
+
+@Component({
+  selector: 'app-hero',
+  standalone: true,
+  imports: [AlbumsSliderComponent],
+  templateUrl: './hero.component.html',
+  styleUrls: ['./hero.component.scss'],
+})
+export class HeroComponent {}

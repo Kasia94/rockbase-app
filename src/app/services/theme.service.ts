636d8421503eab9cbec1de260bb52fcadd3cad15
Katarzyna <katarzyna.skibinska94@gmail.com>
2025-12-02 15:10:04 +0100

    theme

---
+++ src/app/services/theme.service.ts
@@ -0,0 +1,27 @@
+import { Inject, Injectable } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ThemeService {
+  
+  private darkMode = true; 
+
+  constructor(@Inject(DOCUMENT)private document: Document) {
+    document.body.classList.add('dark-mode'); 
+  }
+
+  toggleTheme() {
+    this.darkMode = !this.darkMode;
+    if (this.darkMode) {
+      document.body.classList.add('dark-mode');
+    } else {
+      document.body.classList.remove('dark-mode');
+    }
+  }
+
+  get isDarkMode(): boolean {
+    return this.darkMode;
+  }
+}

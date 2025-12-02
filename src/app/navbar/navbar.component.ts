74c9091776bfd0d4bf02c242fdee78010425e4b9
Katarzyna <katarzyna.skibinska94@gmail.com>
2025-12-02 15:13:03 +0100

    navbar

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { ThemeService } from '../services/theme.service';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
+})
+export class NavbarComponent {
+  constructor(private themeService: ThemeService) {}
+
+  toggleTheme() {
+    this.themeService.toggleTheme();
+  }
+
+  isDarkMode(){
+    return this.themeService.isDarkMode;
+  }
+
+}
